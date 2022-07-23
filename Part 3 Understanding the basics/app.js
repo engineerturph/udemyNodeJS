6360f00c7c4f35559c44bde8731614e2f1b4ae5a
engineerturph <engineerturph@gmail.com>
2022-07-23 16:46:44 +0300

    initial commit

---
+++ Part 3 Understanding the basics/app.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer(function (req, res) {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+      console.log(Buffer.concat(body)[0]);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString(); //asci sayilarini harfe ceviriyor.
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.setHeader("Location", "/");
+        res.statusCode = 302;
+        res.end();
+      });
+    }); //bunu koyunca req a event listener ekliyor ve sonraki isleme geciyor.
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello world</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
