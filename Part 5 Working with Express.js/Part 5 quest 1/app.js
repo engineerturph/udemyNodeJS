1d2561a25755ab01a0416fb2ea7f7f00de3fda42
engineerturph <engineerturph@gmail.com>
2022-07-25 15:37:45 +0300

    part 5 progress

---
+++ Part 5 Working with Express.js/Part 5 quest 1/app.js
@@ -0,0 +1,29 @@
+const http = require("http");
+
+const express = require("express");
+
+const app = express();
+
+app.use("/users", (req, res, next) => {
+  //   console.log("asdfa");
+  res.send(
+    `<head><link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon"></head><p>userpage</p>`
+  );
+});
+
+app.use("/", (req, res, next) => {
+  res.send(
+    `<head><link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon"></head><p>dummytest</p>`
+  );
+  next();
+});
+
+app.use((req, res, next) => {
+  if (req.url.includes("/favicon.ico")) {
+    console.log("asd");
+    return next(); // specify return so the next console.log won't be executed
+  }
+  console.log("asdfa");
+});
+
+app.listen(3000);
--- Part 5 Working with Express.js/app.js
+++ Part 5 Working with Express.js/app.js
@@ -4,10 +4,26 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
+const bodyP = require("body-parser");
+
+app.use(bodyP.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
+  console.log("This always runs");
   next();
 });
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In the middleware!");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Occc</button></form><h1>The "Add Product" Page<h1>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+}); //sadece POST ta calisir.
 
 app.use((req, res, next) => {
   console.log("In another middleware!");
